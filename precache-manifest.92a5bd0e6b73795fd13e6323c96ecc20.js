c026d0fbc6ab82800ada099ada0722995bf72ee1
Nat Rengasamy <81802101+post-war@users.noreply.github.com>
2023-05-28 22:15:33 +1000

    Add files via upload

---
+++ precache-manifest.92a5bd0e6b73795fd13e6323c96ecc20.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "5aea38a4d2296bf533cd",
+    "url": "/static/css/main.c47ef964.chunk.css"
+  },
+  {
+    "revision": "5aea38a4d2296bf533cd",
+    "url": "/static/js/main.5aea38a4.chunk.js"
+  },
+  {
+    "revision": "5b0c1fd61327d865c6b6",
+    "url": "/static/js/1.5b0c1fd6.chunk.js"
+  },
+  {
+    "revision": "9039698ec41d2b4181e9",
+    "url": "/static/js/2.9039698e.chunk.js"
+  },
+  {
+    "revision": "b784ede9eaf89b0e2666",
+    "url": "/static/js/runtime~main.b784ede9.js"
+  },
+  {
+    "revision": "66352371b1323d9dc7ada80cbe9b6c56",
+    "url": "/index.html"
+  }
+];
